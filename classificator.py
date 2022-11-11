6da7a629a2122dfeb0c75b916e45e32e7d6ee18c
Luka <lukablaskovic2000@gmail.com>
2022-11-11 17:45:55 +0100

    added stopWords filter

--- classificator.py
+++ classificator.py
@@ -9,7 +9,7 @@ Dodatno, do kada moram uplatiti navedeni iznos?"""
 
 classes = ["topic", "department"]
 departments = ["Referada", "Studentski centar", "Rektorat", "Knjižnica"]
-topics = ["Upis više godine", "Uplata", "Rok", "Ispis"]
+topics = ["Upis više godine", "Uplata", "Rok", "Ispis s fakulteta", "Prebacivanje predmeta"]
 
 doc = nlp(sample_inquiry)
 
@@ -22,12 +22,15 @@ def tokenize(doc):
 def sentenize(doc):
     return [sent for sent in doc.sents]
 
-async def topicClassifier(inquiry):
-    doc = nlp(inquiry)
-    for topic in topics:
-        doc2 = nlp(topic)
-        print("Inquiry : ", topic, "-->", doc.similarity(doc2))
-    return [{"topic" : topic, "similarity" : round(doc.similarity(nlp(topic)), 3)} for topic in topics]
+def removeStopWords(doc):
+    return nlp(" ".join([str(t) for t in doc if not t.is_stop]))
 
-#print(topicClassifier(doc))
+async def topicClassifier(inquiry):
+    print(inquiry)
+    doc = nlp(inquiry)
+    doc_cleaned = removeStopWords(doc)
+    print(doc)
+    return [{"topic" : topic, "similarity" : round(doc_cleaned.similarity(nlp(topic)), 3)} for topic in topics]
+
+print(topicClassifier(doc))
 #print(tokenize(doc))
